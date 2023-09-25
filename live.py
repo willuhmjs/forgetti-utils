09304f1f04af7d521d25d399ed4c65798acf0aae
William Faircloth <willuhmjs@gmail.com>
2023-09-25 17:57:29 +0000

    feat: basic detection

--- live.py
+++ live.py
@@ -1,8 +1,14 @@
 from ultralytics import YOLO
 import cv2
+import json
 
 model = YOLO("best.pt")
-results = model.predict(source="spaghetti.png", show=True)
-#results = model.predict(source="http://raspberrypi.local/webcam/?action=stream", show=True)
-#results = model.predict(source="https://youtu.be/JGpCGOMgk5g", show=True)
-print(results)
+results = model.predict(source="https://fabbaloo.com/wp-content/uploads/2020/05/image-asset_img_5eb0af2d092e0.jpg", verbose=False, show=False, stream=True)
+for r in results:
+    j = json.loads(r.tojson())
+    if len(j) > 0:
+        confidence = round(j[0]["confidence"], 2)
+        if confidence >= 0.7:
+            print(f'Sure detection found @ {confidence}')
+        elif confidence >= 0.55:
+            print(f'Possible detection found @ {confidence}')
