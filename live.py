a40135353d89dd2be2bf1354e2cb0fe46c33a763
willuhmjs <willuhmjs@gmail.com>
2023-09-24 22:45:41 -0400

    feat: more live

--- live.py
+++ live.py
@@ -1,7 +1,8 @@
 from ultralytics import YOLO
-from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
 
 model = YOLO("best.pt")
-results = model.predict(source="0", show=True)
+results = model.predict(source="spaghetti.png", show=True)
+#results = model.predict(source="http://raspberrypi.local/webcam/?action=stream", show=True)
+#results = model.predict(source="https://youtu.be/JGpCGOMgk5g", show=True)
 print(results)
