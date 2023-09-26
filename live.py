b5c2653df8204ba8bf57f9e4be306229dc3f1c00
willuhmjs <willuhmjs@gmail.com>
2023-09-25 22:54:55 -0400

    feat: save modified image headlessly and extract more info

--- live.py
+++ live.py
@@ -3,9 +3,11 @@ import cv2
 import json
 
 model = YOLO("best.pt")
-results = model.predict(source="https://fabbaloo.com/wp-content/uploads/2020/05/image-asset_img_5eb0af2d092e0.jpg", verbose=False, show=False, stream=True)
+results = model.predict(source="https://fabbaloo.com/wp-content/uploads/2020/05/image-asset_img_5eb0af2d092e0.jpg", verbose=False, show=False, stream=True, conf=0.55)
 for r in results:
     j = json.loads(r.tojson())
+    img = r.plot()
+    cv2.imwrite("test.jpg", img)
     if len(j) > 0:
         confidence = round(j[0]["confidence"], 2)
         if confidence >= 0.7:
